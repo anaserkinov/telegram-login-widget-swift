4aada9a9fe19e405fdd14cfe85d92882b3cc4fd6
anaserkinov <anaserkinjonov@gmail.com>
2026-03-09 04:36:24 +0500

    fix: correct formatting in Package.swift

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .library(
             name: "TelegramLoginWidget",
             targets: ["TelegramLoginWidget"]
-        ),
+        )
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
